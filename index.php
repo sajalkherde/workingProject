64f8d5018cf3e9fdb93e7885656b0f9708784e9b
Sajal Kherde <skherde1@gmail.com>
2017-08-21 09:34:37 -0400

    new files

---
+++ index.php
@@ -0,0 +1,33 @@
+<html>
+<head>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="login.css" type="text/css"> <!--attach CSS file to PHP-->
+  <script type="text/javascript" src="login.js"></script> <!--attach Javascript file to PHP-->
+</head>
+
+
+
+
+<body>
+  <?php
+  if ($_SERVER["REQUEST_METHOD"] == "POST")
+  {
+    session_start();
+    $_SESSION['username'] = $_POST['usernameField']
+    $_SESSION['password'] = $_POST['passwordField']
+  }
+  ?>
+
+  <div id="login">
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+      <input type="text" value="Username" id="usernameField" onfocus="changeUsername()" required>
+      <input type="text" value="Password" id="passwordField" onfocus="changePassword()" required>
+      <input type="submit" id="cred_submit" value="Submit">
+    </form>
+  </div>
+
+
+</body>
+
+
+</html>
